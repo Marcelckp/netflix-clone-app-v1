5a57d69b7a923e41130b05efa60096dfb31243e3
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-08-20 20:37:40 +0200

    create version 1 of the app /home page and create components needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './components/MoviesRow'
+import apiRequest from './apiRequest';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className="app" >
+            <Nav />
+            <Banner />
+            <Row 
+                title='NETFLIX ORIGINALS' 
+                fetchUrl={apiRequest.fetchNetflixOriginals} 
+                originalsRow={true}
+                />
+            <Row title='Trending' fetchUrl={apiRequest.fetchTrending} />
+            <Row title='Top Rated' fetchUrl={apiRequest.fetchTopRated} />
+            <Row title='Action Movies' fetchUrl={apiRequest.fetchActionMovies} />
+            <Row title='Comedy Movies' fetchUrl={apiRequest.fetchComedyMovies} />
+            <Row title='Horror Movies' fetchUrl={apiRequest.fetchHorrorMovies} />
+            <Row title='Romance Movies' fetchUrl={apiRequest.fetchRomanceMovies} />
+            <Row title='Documentaries' fetchUrl={apiRequest.fetchDocumentaries} />
+        </div>
+    );
 }
 
 export default App;
